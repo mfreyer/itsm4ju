226e11f9a15bf405e7c874d039606f72430437d2
U-CHBSPORT03\Matthias <Matthias@chbsport03.(none)>
2011-10-30 00:59:56 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itsm4ju::Application.config.secret_token = 'b847f6b2bb647a5adc55df6d7f642181a3bbb244c8911fb18f9777ba1e19d4e54aa43575d94beb33cd5490c727dc7506462ce3486e8cc96ed2ee868a3fee6f74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Itsm4ju::Application.config.session_store :cookie_store, :key => '_itsm4ju_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Itsm4ju::Application.config.session_store :active_record_store
